844c45619ec9481d05e372c07c32ad7ad5ebd84c
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2023-10-19 00:50:00 +0530

    added tailwind css

--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div
+        className="bg-cover bg-[#1F1F1F] bg-center bg-no-repeat h-screen"
+      >
+            <h1 className="text-4xl text-green-600 ">Dashboard</h1>
+    </div>
   )
 }
 
